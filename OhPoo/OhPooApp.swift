050b10687a67ca9e90effa112415cfe2360a182f
Carlo Jacob <carlo.jacob@gmail.com>
2024-01-30 10:40:47 -0500

    Initial commit

---
+++ OhPoo/OhPooApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhPooApp.swift
+//  OhPoo
+//
+//  Created by Carlo Jacob on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct OhPooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
