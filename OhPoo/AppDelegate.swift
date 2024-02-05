2bda9d410e3ac8a815efc7cf95506d30b8e81bc9
Carlo Jacob <carlo.jacob@gmail.com>
2024-02-05 18:07:19 -0500

    Add ApplDelegate adapter
    - Allows audio to play in the background.
    - Allows audio to play with Silent Mode selected.

---
+++ OhPoo/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  OhPoo
+//
+//  Created by Carlo Jacob on 2/5/24.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		let audioSession = AVAudioSession.sharedInstance()
+
+		do {
+			try audioSession.setCategory(.playback, options: .mixWithOthers)
+			print("Audio set up fine")
+			try audioSession.setActive(true)
+		} catch {
+			print("Unable to set up audio player: \(error.localizedDescription)")
+		}
+		return true
+	}
+}
